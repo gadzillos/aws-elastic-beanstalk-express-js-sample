cd4ff32f8fcc89779dc9310cdd5b2d6438783828
Saewran <saewran@pop-os.localdomain>
2021-09-24 14:10:38 +0300

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Ohaio sekai'));
+app.get('/', (req, res) => res.send('Ohaio sekai - Good morning World!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
